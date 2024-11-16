dd64d1a4df25ec95746f56e4128878c3547c7375
solp721 <easter721@gmail.com>
2024-11-17 06:25:59 +0900

    feat : App 컴포너트

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Content from "./Content";
+
+export default function App() {
+  return (
+    <div id="app">
+      <Header title="리액트 만들어보기" />
+      <Content description="Virtual DOM 이해하기" />
+    </div>
+  );
+}
