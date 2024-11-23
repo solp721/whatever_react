eb0a9e8cd69526b2f8a605ecc4474c73f1d771df
solp721 <easter721@gmail.com>
2024-11-24 01:43:18 +0900

    feat : 카운터 컴포넌트 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Content from "./Content";
+import Counter from "./Counter";
 
 export default function App() {
   return (
     <div id="app">
       <Header />
       <Content />
+      <Counter />
     </div>
   );
 }
