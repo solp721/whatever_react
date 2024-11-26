7db8e8f11b8df7bde19ebbb0bec4dcb3bb33d533
solp721 <easter721@gmail.com>
2024-11-26 22:24:51 +0900

    fix : week2 솔루션 및 해답 참고후 todo 리스트 변경

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,31 +5,22 @@ export default function Todo() {
   const [input, setInput] = useState("");
 
   function addTodo() {
-    if (input.trim()) {
-      setTodos([...todos, { text: input, completed: false }]);
-      setInput("");
-    }
-  }
-
-  function toggleTodo(index) {
-    const newTodos = todos.slice();
-    newTodos[index].completed = !newTodos[index].completed;
-    setTodos(newTodos);
+    setTodos([...todos, input]);
+    setInput("");
   }
 
   return (
     <div>
-      <h2>TODO 리스트</h2>
+      <h2>할 일 목록</h2>
       <input
         type="text"
         value={input}
         oninput={(e) => setInput(e.target.value)}
-        placeholder="할 일을 입력하세요"
       />
       <button onclick={addTodo}>추가</button>
       <ul>
-        {todos.map((todo, index) => (
-          <li onclick={() => toggleTodo(index)}>{todo.text}</li>
+        {todos.map((todo) => (
+          <li>{todo}</li>
         ))}
       </ul>
     </div>
