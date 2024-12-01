6e0956638f76943051f6df3420c7b30bef4e4298
solp721 <easter721@gmail.com>
2024-12-01 13:25:19 +0900

    feat : 타이머 컴포넌트 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import Header from "./Header";
 import Content from "./Content";
 import Counter from "./Counter";
 import Todo from "./Todo";
+import Timer from "./Timer";
 
 export default function App() {
   return (
     <div id="app">
       {/* <Header /> */}
       {/* <Content /> */}
-      <Counter />
-      <Todo />
+      {/* <Counter /> */}
+      {/* <Todo /> */}
+      <Timer />
     </div>
   );
 }
