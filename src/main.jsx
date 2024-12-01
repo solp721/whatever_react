2bd7dcffc35e4c29e9b83ce62378509433eceffb
solp721 <easter721@gmail.com>
2024-12-01 13:25:36 +0900

    feat : useEffect 인덱스 초기화

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { render } from "./core/render";
 import App from "./components/App";
 import { resetStateIndex, setRerenderFunc } from "./core/hooks/useState";
+import { resetEffectIndex } from "./core/hooks/useEffect";
 import { addEventDelegation } from "./core/addEventDelegation";
 
 const container = document.getElementById("app");
@@ -9,10 +10,11 @@ const container = document.getElementById("app");
  * @description 애플리케이션 렌더링
  */
 function renderApp() {
-  resetStateIndex();
+  resetStateIndex(); // useState 인덱스 초기화
+  resetEffectIndex(); // useEffect 인덱스 초기화
   const appElement = App(); // Virtual DOM 생성
   render(appElement, container); // Virtual DOM 렌더링
-  console.log(JSON.stringify(appElement, null, 2));
+  // console.log(JSON.stringify(appElement, null, 2));
 }
 
 // 이벤트 위임 활성화
